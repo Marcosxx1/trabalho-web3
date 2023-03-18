b4e57391c214b2651ec9f5a512a6302e23a8fdfd
Luiz Neto <100620915+LuizFNeto20@users.noreply.github.com>
2023-03-18 11:40:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-    include_once "autoload.php";
-    define("APP", "http://localhost/TrabalhoWeb3/");
+include_once "autoload.php";
+define("APP", "http://localhost/TrabalhoWeb3/");
+if (!isset($_Get["url"])) {
+    $url = "index/index";
+} else {
+    $url = $_Get["url"];
+}
 
+$vetor_url = explode("/", $url);
+$name_controller = ucfirst($vetor_url[0]) . 'Controller';
+$acao = $vetor_url[1];
+
+if (count($vetor_url) == 2) {
+    $controller->$acao();
+} else {
+    $id = $vetor_url[2];
+    $controller->$acao($id);
+}
 ?>
