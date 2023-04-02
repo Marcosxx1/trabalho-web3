0e839179d0f2894249984c557e60ae3e667cae38
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-04-02 09:51:53 -0400

    Inicio do registro de usuários

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -2,5 +2,25 @@
   class Usuario extends Model {
     protected $tabela="usuario";
     protected $query = "SELECT usuario.nome, usuario.cep, usuario.id, usuario.numero_casa, usuario.email FROM usuario";
+
+
+  public function registrar()
+  {
+    //Checa se tem algo no POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    } else {
+
+      $data = [
+        'nome' => '',
+        'cep' => '',
+        'numero_casa' => '',
+        'email' => '',
+        'senha' => ''
+      ];
+
+      $this->view('users/register');
+    }
   }
+  }
+ 
  ?>
